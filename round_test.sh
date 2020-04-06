8f6aef09400154a376b71c0f619b6509bc6dd38e
david27941 <jimsam3456@cycu.org.tw>
2020-04-06 11:11:38 -0500

    change the setting of round_test.sh

--- round_test.sh
+++ round_test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-for i in {1..5};
+for i in {1..600..10};
 do
-NUM=$[5 * $i]
-./run.sh -t streaming_classify -m resnet50 -ns $NUM -g -c throughput -d $HOME/CK-TOOLS/dataset-imagenet-ilsvrc2012-val-min | cat >> log4.txt 
+NUM=$[10 * $i]
+./run.sh -t streaming_classify -m resnet50 -s $NUM -g -c throughput -d $HOME/CK-TOOLS/dataset-imagenet-ilsvrc2012-val-min | cat >> log4.txt 
 
 done
