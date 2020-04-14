a7c617dde76889e492a9874387172839edab7dc9
davidho27941 <jimsam3456@cycu.org.tw>
2020-04-14 04:25:02 -0600

    update a new version of round_test.sh, also add new test dataset

--- round_test.sh
+++ round_test.sh
@@ -1,7 +1,31 @@
 #!/bin/bash
-for i in {1..600..10};
+
+if [ "${1}" == "1" ]; then
+mode1=streaming_classify
+else
+mode1=streaming_classify_fpgaonly
+fi
+
+if [ "${2}" == "1" ]; then
+mode2=autoAllOpt
+elif [ "${2}" == "2" ]; then 
+mode2=throughput
+else 
+mode2=latency
+fi
+
+for a in {1..5..1};
+do 
+
+start=$SECONDS
+for i in {10..60..10};
 do
 NUM=$[10 * $i]
-./run.sh -t streaming_classify -m resnet50 -s $NUM -g -c throughput -d $HOME/CK-TOOLS/dataset-imagenet-ilsvrc2012-val-min | cat >> log4.txt 
+./run.sh -t $mode1 -m resnet50 -s $NUM  -c $mode2 -d $HOME/CK-TOOLS/dataset-imagenet-ilsvrc2012-val-min | cat > FNAL-Alveo-FPGA/$mode1/UCSD/$mode2/log$a.txt 
 
+done
+
+duration=$(( SECONDS - start ))
+echo $duration  >> FNAL-Alveo-FPGA/$mode1/UCSD/$mode2/timer$a.txt
+echo $duration
 done
