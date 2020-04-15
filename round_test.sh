6ddc4c0f1d5995fae61435341b7bba13f9acfa26
davidho27941 <davidho@ailab01.fnal.gov>
2020-04-15 01:14:24 -0600

    delete previous data, change round_test.sh

--- round_test.sh
+++ round_test.sh
@@ -1,31 +1,80 @@
 #!/bin/bash
 
-if [ "${1}" == "1" ]; then
-mode1=streaming_classify
+if [ "${1}" == "1" ]; then 
+cond=Switch_batch_size
+
+	if [ "${2}" == "1" ]; then
+		mode1=streaming_classify
+	elif [ "2" == "2" ]; then
+		mode1=streaming_classify_fpgaonly
+	else 
+		echo "Please select a mode to run. (1 = streaming_classify, 2 = streaming_classify_fpgaonly)"
+	fi
+
+	if [ "${3}" == "1" ]; then
+		mode2=autoAllOpt
+	elif [ "${3}" == "2" ]; then 
+		mode2=throughput
+	elif [ "$3" == "3" ]; then
+		mode2=latency
+	else 
+		echo "Please select a mode to test.(1 = autoAllOpt, 2 = throughput, 3 = latency )"
+	fi
+
+	for a in {1..5..1};
+	do 
+
+		start=$SECONDS
+		for i in {10..60..10};
+		do
+			NUM=$[1 * $i]
+			./run.sh -t $mode1 -m resnet50 -s $NUM  -c $mode2 -d $HOME/CK-TOOLS/dataset-imagenet-ilsvrc2012-val-min | cat >> FNAL-Alveo-FPGA/$cond/$mode1/FNAL/$mode2/log$a.txt 
+		done
+
+		duration=$(( SECONDS - start ))
+		echo $duration  >> FNAL-Alveo-FPGA/$cond/$mode1/FNAL/$mode2/timer$a.txt
+
+		echo $duration
+	done
+
+elif [ "${1}" == "2" ]; then
+	cond=Switch_num_of_stream
+
+	if [ "${2}" == "1" ]; then
+		mode1=streaming_classify
+	elif [ "2" == "2" ]; then
+		mode1=streaming_classify_fpgaonly
+	else
+		echo "Please select a mode to run. (1 = streaming_classify, 2 = streaming_classify_fpgaonly)"
+	fi
+
+	if [ "${3}" == "1" ]; then
+		mode2=autoAllOpt
+	elif [ "${3}" == "2" ]; then
+		mode2=throughput
+	elif [ "$3" == "3" ]; then
+		mode2=latency
+	else
+                echo "Please select a mode to test.(1 = autoAllOpt, 2 = throughput, 3 = latency )"
+	fi
+
+	for a in {1..5..1};
+	do
+
+		start=$SECONDS
+		for i in {1..5};
+		do
+			NUM=$[4 * $i]
+			./run.sh -t $mode1 -m resnet50 -ns $NUM  -c $mode2 -d $HOME/CK-TOOLS/dataset-imagenet-ilsvrc2012-val-min | cat >> FNAL-Alveo-FPGA/$cond/$mode1/FNAL/$mode2/log$a.txt
+		done
+
+		duration=$(( SECONDS - start ))
+		echo $duration  >> FNAL-Alveo-FPGA/$cond/$mode1/FNAL/$mode2/timer$a.txt
+
+		echo $duration
+	done
+
 else
-mode1=streaming_classify_fpgaonly
+echo "Please select a condition to run. (1 = Switch_batch_size, 2 = Switch_num_of_stream)"
 fi
 
-if [ "${2}" == "1" ]; then
-mode2=autoAllOpt
-elif [ "${2}" == "2" ]; then 
-mode2=throughput
-else 
-mode2=latency
-fi
-
-for a in {1..5..1};
-do 
-
-start=$SECONDS
-for i in {10..60..10};
-do
-NUM=$[10 * $i]
-./run.sh -t $mode1 -m resnet50 -s $NUM  -c $mode2 -d $HOME/CK-TOOLS/dataset-imagenet-ilsvrc2012-val-min | cat > FNAL-Alveo-FPGA/$mode1/UCSD/$mode2/log$a.txt 
-
-done
-
-duration=$(( SECONDS - start ))
-echo $duration  >> FNAL-Alveo-FPGA/$mode1/UCSD/$mode2/timer$a.txt
-echo $duration
-done
--- streaming_classify_fpgaonly/FNAL/autoAllOpt/grep.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-grep 'FPGA_process' log.txt >> test.txt
-grep 'FPGA_process' log1.txt >> test1.txt
-grep 'FPGA_process' log2.txt >> test2.txt
-grep 'FPGA_process' log3.txt >> test3.txt
-grep 'FPGA_process' log4.txt >> test4.txt
