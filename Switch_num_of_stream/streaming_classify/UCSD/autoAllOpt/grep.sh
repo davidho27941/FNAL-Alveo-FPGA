578eb6f6b1743e4b16cd5d8d73df0a6849251669
davidho27941 <davidho@ailab01.fnal.gov>
2020-04-15 02:39:58 -0600

    update

---
+++ Switch_num_of_stream/streaming_classify/UCSD/autoAllOpt/grep.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+grep 'FPGA_process' log.txt >> test.txt
+grep 'FPGA_process' log1.txt >> test1.txt
+grep 'FPGA_process' log2.txt >> test2.txt
+grep 'FPGA_process' log3.txt >> test3.txt
+grep 'FPGA_process' log4.txt >> test4.txt
