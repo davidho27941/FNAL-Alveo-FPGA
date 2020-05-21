82a7438608304906274568782ad19fb1aa05a045
davidho27941 <jimsam3456@cycu.org.tw>
2020-05-21 15:06:16 +0000

    add aux file

--- setup.sh
+++ setup.sh
@@ -2,4 +2,5 @@
 
 python -m ck pull repo:ck-env
 python -m ck install package:imagenet-2012-val-min
-
+python -m ck install package:imagenet-2012-aux
+head -n 500 ~/CK-TOOLS/dataset-imagenet-ilsvrc2012-aux/val.txt > ~/CK-TOOLS/dataset-imagenet-ilsvrc2012-val-min/val.txt
